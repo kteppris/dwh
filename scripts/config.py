647f251af9d889ab687c145e4be10274a5e8cf22
kteppris <66556136+kteppris@users.noreply.github.com>
2022-05-23 15:29:00 +0200

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -2,4 +2,4 @@
 
 # Scheme: "postgresql+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 
-DATABASE_URI = 'postgresql+psycopg2://postgres:dolomit900@localhost:5432/DataScience'
+DATABASE_URI = 'postgresql+psycopg2://postgres:<Password>@localhost:5432/<Database>'
